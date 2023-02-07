cbb444b2f77b5b1cedc547a9da9b48f9477e69ea
eastpiada <280397+eastpiada@users.noreply.github.com>
2023-02-07 10:25:10 -0500

    twitz

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -62,7 +62,7 @@
     <Delegate/>
     <div class="w3-xlarge w3-section">
       <span class="p-p3">&#960; &#43; &#8371; &#61; &#8766;</span>
-      <a href="https://twitter.com/Piada_stakePool" rel="noreferrer" target="_blank"><i class="fa-brands fa-twitter w3-hover-opacity p-p3"></i></a>
+      <a href="https://twitter.com/eastpiada" rel="noreferrer" target="_blank"><i class="fa-brands fa-twitter w3-hover-opacity p-p3"></i></a>
       <a href="https://t.me/piadapool" rel="noreferrer" target="_blank"><i class="fa-brands fa-telegram w3-hover-opacity p-p3"></i></a>
       <a href="https://armada-alliance.com/" rel="noreferrer" target="_blank"><img class="w3-hover-opacity" src={aaShip} alt="Armada Alliance Logo" style="height:30px;position:relative;top:-1px;"></a>
     </div>
